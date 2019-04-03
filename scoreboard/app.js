e4d6c0dbc21d70d2f71d1514f8daf88c93d6b881
daesy13 <daesyramos@hotmail.com>
2019-04-03 22:16:03 +0000

    my first REACT

---
+++ scoreboard/app.js
@@ -0,0 +1,12 @@
+const title = React.createElement(
+    'h1',
+    { id: 'main-title', title: 'This is a title' },
+    'My First React Element!'
+);
+
+// <h1 id="main-title" title="This is a title">My First React Element!</h1>
+
+ReactDOM.render(
+    title,
+    document.getElementById('root')
+)
